7fdc19eb60dc343775dba774d815ef407e4e8483
Supun Abeysinghe <supunabeysinghe@gmail.com>
2022-10-20 22:00:54 -0400

    verify output

--- very_simple_ait_custom_op/load_ait_custom_op.py
+++ very_simple_ait_custom_op/load_ait_custom_op.py
@@ -42,6 +42,31 @@ def create_model(path):
     model = helper.make_model(graph, producer_name="ait-customop-example")
     save(model, path)
 
+
+def torch_output(input_data, weight_data):
+    import torch
+    import torch.nn as nn
+    
+    class VerySimpleModel(nn.Module):
+        def __init__(self, hidden_size) -> None:
+            super().__init__()
+            self.linear = nn.Linear(hidden_size, hidden_size, bias=False)
+        
+        def forward(self, input):
+            hidden_states = self.linear(input)
+            return hidden_states
+
+    model = VerySimpleModel(hidden_size).half().cuda().eval()
+    input_torch = torch.from_numpy(input_data).cuda()
+    assert(input_torch.dtype == torch.float16) # make sure data type is preserved
+    weight_torch = torch.from_numpy(weight_data).cuda()
+    assert(weight_torch.dtype == torch.float16)
+
+    model.linear.weight.data = weight_torch
+    output = model(input_torch)
+    return output
+
+
 if __name__ == "__main__":
     shared_library = "./test.so" # was compiled from the docker container. Should be fine since it's the same OS?
 
@@ -75,5 +100,15 @@ if __name__ == "__main__":
                         element_type=np.float16, shape=output_ortvalue.shape(), buffer_ptr=output_ortvalue.data_ptr())
 
     session.run_with_iobinding(io_binding)
-    res = output_ortvalue.numpy()
-    print(res)
+    ait_output = output_ortvalue.numpy()
+    torch_output = torch_output(input_data, weight_data).cpu().detach().numpy()
+
+    equal = np.allclose(ait_output, torch_output, atol=1e-1)
+
+    # print(ait_output)
+    # print(torch_output)
+
+    if equal:
+        print("Outputs matched! (to a 0.1 tolerenace")
+    else:
+        print("ERROR! outputs are different!")
